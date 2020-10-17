66c0f78ac31ec072805f2219353dcd5f1351e609
Mithun <joieeemithun98@gmail.com>
2020-10-17 21:07:13 +0530

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Homepage from './pages/home';
+import { Switch, Route } from 'react-router-dom';
+
+
+const HatsPage = () => (
+  < div >
+    <h1>Hats Page</h1>
+  </div >
+);
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../components/directory/directory'
+// import MenuItem from '../components/menu-item/ourmenu'
+
+const Homepage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default Homepage
