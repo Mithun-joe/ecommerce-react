1fc49dc57ee96154733cbfb9f6a4c30f7442c76f
Mithun <joieeemithun98@gmail.com>
2020-10-18 18:15:50 +0530

    user auth updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Homepage from './pages/homepage/home';
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop'
 import Header from "./components/header/header";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUp} />
       </Switch>
     </div>
   );
---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './sign-in.style.scss';
+
+class SignIn extends React.Component{
+    constructor() {
+        super();
+
+        this.state ={
+            email:'',
+            password:'',
+        }
+    }
+
+    handleSubmit = (e) =>{
+        e.preventDefault();
+
+        this.setState({'email':'','password':''})
+    };
+
+    handleChange = (e) =>{
+        const {value ,name } = e.target;
+
+        this.setState({[name]:value})
+    };
+
+
+    render(){
+        return(
+            <div className='sign-in'>
+                <h2>I already have a account</h2>
+                <span>Sign in with your email and password</span>
+
+                <form onSubmit={this.handleSubmit}>
+                    <input name='email' type='email' onChange={this.handleChange} value={this.state.email} required />
+                    <label>Email</label>
+                    <input name='password' type='password' onChange={this.handleChange} value={this.state.password} required />
+                    <label>Password</label>
+
+                    <input type='submit' value='Submit Form'/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default  SignIn;
