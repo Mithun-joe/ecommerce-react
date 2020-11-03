b6a60f3a205fdfd01c20d8011b49c7d07e80caac
Mithun <joieeemithun98@gmail.com>
2020-11-03 18:15:30 +0530

    updated now

--- src/components/item-collection/item.collection.jsx
+++ src/components/item-collection/item.collection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './item.collection.style.scss';
+import CustomButton from "../custom-button/custom-button";
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
     <div className='collection-item'>
@@ -11,6 +12,7 @@ const CollectionItem = ({id, name, price, imageUrl}) => (
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
+        <CustomButton inverted> Add To Cart </CustomButton>
     </div>
 );
 
