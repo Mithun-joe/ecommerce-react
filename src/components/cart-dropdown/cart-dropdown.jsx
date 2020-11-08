a1ba0c1ad0d9262fba8eb5c0d4f68747474128ff
Mithun <joieeemithun98@gmail.com>
2020-11-08 21:21:39 +0530

    updated redux selectors in cart items

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -3,6 +3,7 @@ import CustomButton from "../custom-button/custom-button";
 import './cart-dropdown-styles.scss';
 import CartItem from "../cart-item/cart-item";
 import {connect} from 'react-redux'
+import {selectCartItems} from "../../redux/cart/cart.selectors";
 
 
 const CartDropDown = ({cartItems}) => (
@@ -18,8 +19,8 @@ const CartDropDown = ({cartItems}) => (
     </div>
 );
 
-const mapStateToProps = ({cart:{cartItems} }) => ({
-    cartItems
-})
+const mapStateToProps = state => ({
+    cartItems: selectCartItems(state)
+});
 
 export default connect(mapStateToProps)(CartDropDown);
